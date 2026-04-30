86ba9b9b48f6dd007ba236568eb9734ccc697181
anand3559 <anandyadav3559@gmail.com>
2026-04-30 14:29:19 +0530

    some changes

--- main.py
+++ main.py
@@ -130,4 +130,6 @@ async def reply(body: ReplyBody):
     return action
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    # reload=True is removed for production deployment (Railway)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
